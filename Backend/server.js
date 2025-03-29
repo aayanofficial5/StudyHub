127b1b61416870e9222e6300bff353be186e6085
aayanofficial5 <aayanofficial5@gmail.com>
2025-03-29 23:34:37 +0530

    Backend setup

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dbConnect = require("./Config/database");
+const router = require("./Routes/router");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 4000; // Use PORT for the server
+
+// request body json data parser
+app.use(express.json());
+
+// mapping of routes with app
+app.use("/api/v1", router);
+
+// server starting
+app.listen(port, () => {
+  console.log(`server is running on port ${port}...`);
+});
+
+// database connection
+dbConnect();
