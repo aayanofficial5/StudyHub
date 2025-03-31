00ac6e715adc24c61228586c7f6407235fd212da
aayanofficial5 <aayanofficial5@gmail.com>
2025-03-31 16:35:43 +0530

    user model done

--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -1,7 +1,55 @@
 const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({
-  // write data structure of schema
-})
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+  accountType: {
+    type: String,
+    enum: ["Admin", "Student", "Instructor"],
+    required: true,
+  },
+  additionalDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Profile",
+    required: true,
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+  image: {
+    type: String,
+    required: true,
+  },
+  courseProgress: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CourseProgress",
+    },
+  ],
+});
 
-module.exports = mongoose.model("Model",schema);
+module.exports = mongoose.model("User", userSchema);
