e1585f9df44791c7f38075bdddf53e7847df0222
aayanofficial5 <aayanofficial5@gmail.com>
2025-04-02 01:44:22 +0530

    sendOtp controller done

---
+++ Backend/Controllers/auth.js
@@ -0,0 +1,44 @@
+const OTP = require("../Models/OTP");
+const User = require("../Models/User");
+const otpGenerator = require("otp-generator");
+
+exports.sendOTP = async (req, res) => {
+  try {
+    const { email } = req.body;
+    // check if user exists
+    if (await User.findOne({ email })) {
+      return res.status(400).json({
+        success: false,
+        message: "User Already Exists with Given Email.",
+      });
+    }
+    do {
+      // generate otp
+      const otp = otpGenerator.generate(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      });
+      // check otp is unique or not
+      let result = await OTP.findOne({ otp });
+    } while (result);
+    console.log(result);
+
+    const otpPayload = { email, otp };
+    // create a OTP document in database
+    const document = await OTP.create(otpPayload);
+    console.log(document);
+    res.status(200).json({
+      success: true,
+      data: document,
+      message: "OTP sent Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to send OTP",
+    });
+  }
+};
+
+exports.signup = async (req, res) => {};
