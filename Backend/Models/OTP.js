dfed2ae2360bcc4312cb45c10dbe67c6aba65593
aayanofficial5 <aayanofficial5@gmail.com>
2025-04-02 01:44:41 +0530

    pre middleware to send email added

--- Backend/Models/OTP.js
+++ Backend/Models/OTP.js
@@ -13,15 +13,27 @@ const OTPSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now(),
+    default: Date.now(),    // date are create in ISO format in mongoose but have a different time zone 
     expires: 5 * 60, // document delete automatically after 5 mins
   },
 });
 
-async function sendVerificationEmail(email, otp) {}
+// adding mongoose pre middleware
 
-OTPSchema.pre("save", async function (next) {
-  await sendVerificationEmail(this.email, this.otp);
+const sendVerificationEmail = async (email,otp)=>{
+  try{
+    const mailResponse = await mailSender(email,"OTP Verification Email","<h2>This Otp is only valid for 5mins : </h2><h3>"+otp+"</h3>");
+    console.log("Email sent successfully...");
+    console.log(mailResponse);
+  }catch(err){
+    console.log("Error occurred while sending email : "+err.message);
+  }
+}
+
+
+OTPSchema.pre("save",async function(next){
+  await sendVerificationEmail(this.email,this.otp);
   next();
-});
+})
+
 module.exports = mongoose.model("OTP", OTPSchema);
