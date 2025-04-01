b309bdb2e3a1b462afd650ef147ce379af11a968
aayanofficial5 <aayanofficial5@gmail.com>
2025-04-02 01:44:53 +0530

    sendotp router added

--- Backend/Routes/router.js
+++ Backend/Routes/router.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { sendOTP } = require("../Controllers/auth");
+
 
 const router = express.Router();
 // write routes here
+
+router.post("/sendOTP",sendOTP);
 
 module.exports = router;
