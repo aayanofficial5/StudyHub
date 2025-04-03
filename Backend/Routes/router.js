c416b6437414d76d4c4cf25ec318be1188852049
aayanofficial5 <aayanofficial5@gmail.com>
2025-04-03 21:57:41 +0530

    routes updated

--- Backend/Routes/router.js
+++ Backend/Routes/router.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { sendOTP } = require("../Controllers/auth");
-
+const { sendOTP, signup, login } = require("../Controllers/auth.js");
 
 const router = express.Router();
 // write routes here
 
-router.post("/sendOTP",sendOTP);
+router.post("/sendOTP", sendOTP);
+router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
