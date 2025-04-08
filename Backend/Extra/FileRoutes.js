d8f7900e77b8dd5c27ec9211ced15bb59637da42
aayanofficial5 <aayanofficial5@gmail.com>
2025-04-08 23:30:02 +0530

    extra arranged

---
+++ Backend/Extra/FileRoutes.js
@@ -0,0 +1,6 @@
+//REVIEW: testing routes
+// router.post("/test-auth", auth, (req, res) => {
+//   res.send("This is Protected route for testing");
+// });
+// router.post("/local-file-upload",localFileUpload);
+// router.post("/cloud-file-upload",auth,cloudFileUpload);
