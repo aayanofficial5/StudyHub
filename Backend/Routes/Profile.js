6b9e08b5f9efdeee88001cf86543dd07f3d623e1
aayanofficial5 <aayanofficial5@gmail.com>
2025-04-10 00:39:45 +0530

    route tested

--- Backend/Routes/Profile.js
+++ Backend/Routes/Profile.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { auth } = require("../Middlewares/Authentication/auth");
 const {
   updateProfilePicture,
   updateProfile,
@@ -11,10 +10,10 @@ const {
 
 // routes
 
-router.patch("/update-password", auth, updatePassword);
-router.delete("/delete-account", auth, deleteAccount);
-router.put("/update-profile-picture", auth, updateProfilePicture);
-router.put("/update-profile", auth, updateProfile);
-router.get("/get-user-details", auth, getUserDetails);
+router.patch("/password", updatePassword);
+router.delete("/", deleteAccount);
+router.put("/picture", updateProfilePicture);
+router.put("/", updateProfile);
+router.get("/me", getUserDetails);
 
 module.exports = router;
--- Backend/Routes/User.js
+++ Backend/Routes/User.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { sendOTP, login, signup } = require("../Controllers/Auth");
-const {resetPassword, resetPasswordToken} = require("../Controllers/ResetPassword")
+const { resetPassword, resetToken } = require("../Controllers/ResetPassword");
 
 const router = express.Router();
 
 // write routes here
-router.post("/send-otp", sendOTP);
+router.post("/otp", sendOTP);
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/reset-password-token", resetPasswordToken);
+router.post("/reset-token", resetToken);
 router.post("/reset-password", resetPassword);
 // router.post("/logout", logout);
 
