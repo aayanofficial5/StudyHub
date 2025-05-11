1cedf8da674d54218b91da92affcdb9e6a10b5e0
aayanofficial5 <aayanofficial5@gmail.com>
2025-05-11 23:59:47 +0530

    Update CTAButton component: adjust button alignment and width for improved layout

--- Client/src/components/Home/CTAButton.jsx
+++ Client/src/components/Home/CTAButton.jsx
@@ -4,7 +4,7 @@ import { FaSignOutAlt } from "react-icons/fa";
 const CTAButton = ({ active, text, arrow, logout, action , children}) => {
   return (
     <button
-      className={`flex flex-row gap-2 items-center w-max transition-all duration-200 ease-in hover:scale-95 px-4 py-2 cursor-pointer rounded-lg border-blue-500 border-3 font-semibold ${
+      className={`flex flex-row gap-2 items-center justify-center w-full transition-all duration-200 ease-in hover:scale-95 px-4 py-2 cursor-pointer rounded-lg border-blue-500 border-3 font-semibold ${
         active && "bg-blue-500"
       }`}
       onClick={action}
