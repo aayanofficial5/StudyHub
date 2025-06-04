78d0131cb8e11a50fc735b24bb4d1585aef122bc
aayanofficial5 <aayanofficial5@gmail.com>
2025-06-04 23:43:50 +0530

    Fix import case sensitivity for User model and add payment routes for capturing, verifying payments, and sending success emails

--- Server/Routes/Payments.js
+++ Server/Routes/Payments.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../Controllers/Payment");
 const router = express.Router();
 
 
 // routes
 
+router.post("/capture", capturePayment);
+router.post("/verify", verifyPayment);
+router.post("/success-email", sendPaymentSuccessEmail);
 
 
 module.exports= router;
